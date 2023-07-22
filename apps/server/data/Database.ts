6f6eeff315737bc2e9e7657e9f1162c0100ef606
rhett-williams <rhettjwilliams@gmail.com>
2023-07-22 10:43:59 +1000

    database functions created

--- apps/server/data/Database.ts
+++ apps/server/data/Database.ts
@@ -1,4 +1,5 @@
-import { generateProductData } from './generator';
+import { faker } from '@faker-js/faker';
+import { Product, generateProductData } from './generator';
 
 /**
  * Treat this as a fake local database.
@@ -7,7 +8,58 @@ import { generateProductData } from './generator';
 export class Database {
   private static data = generateProductData();
 
-  static async getProducts() {
-    return this.data.products;
+  static async getProducts(sortedBy: 'createdDate' | 'price') {
+    const allProducts = this.data.products;
+    switch(sortedBy){
+      case 'createdDate': return allProducts.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
+      case 'price': return allProducts.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+      default: return allProducts
+    }
   }
+
+  static async getOneProduct(productId: string) {
+    const allProducts = this.data.products;
+    const product = allProducts.find((product) => product._id === productId)
+    return product
+  }
+
+  static async createProduct(product: Product) {
+    const newProduct = {
+      ...product,
+      _id: faker.database.mongodbObjectId(),
+    }
+    this.data.products.push(newProduct)
+  }
+
+  static async deleteProduct(productId: string) {
+    const allProducts = this.data.products;
+    const allOrders = this.data.orders;
+
+    for (let i = allProducts.length - 1; i >= 0; i--) {
+      if (allProducts[i]._id === productId) {
+        this.data.products.splice(i, 1);
+      }
+    }
+  
+    for (let i = allOrders.length - 1; i >= 0; i--) {
+      if (allOrders[i].product_id === productId) {
+        this.data.orders.splice(i, 1);
+      }
+    }
+  }
+
+  static async getOrder(orderId: string) {
+    const allOrders = this.data.orders
+    const order = allOrders.find((order) => order._id === orderId)
+    return order
+  }
+
+  static async createOrder(productId: string) {
+    const newOrder = {
+      _id: faker.database.mongodbObjectId(),
+      product_id: productId
+    }
+    this.data.orders.push(newOrder)
+  }
+
 }
